af9e48e440aa73f1da187ba749b44b9500961dbe
Emad Uddin <emaduddin678@gmail.com>
2022-08-19 19:36:50 +0600

    func

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Board from "./board";
 
 class Game extends React.Component {
-    render() {
-        return (
-          <div>
-            <h1>Game Component</h1>
-            <Board />
-          </div>
-        );
-    }
+  state = {
+    history: [{ squares: Array(9).fill(null) }],
+  };
+  onClick = (i) => {
+    console.log(i);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Game Component</h1>
+        <Board squares={this.state.history[0]} onClick={this.onClick} />
+      </div>
+    );
+  }
 }
 
 
 export default Game;
+ 
